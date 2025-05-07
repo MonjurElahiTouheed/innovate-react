cf17f0a902972b97dfbf38092e49295621979613
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-05-07 08:44:19 +0600

    background image used

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import logo from '../../assets/logo.png';
+import Container from '../Layout/Container';
+const Navbar = () => {
+    return (
+        <div className='py-8 absolute w-full'>
+            <Container>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <img src={logo} alt="" />
+                    </div>
+                    <ul className='flex gap-x-[50px] font-secondary text-lg font-medium text-white'>
+                        <li>Home</li>
+                        <li>Features</li>
+                        <li>Service</li>
+                        <li>Pages</li>
+                        <li>Blog</li>
+                    </ul>
+                    <div className='flex gap-x-9 items-center'>
+                        <p className='font-secondary text-lg font-medium text-white'>Login</p>
+                        <button className='px-12 py-5 bg-secondary rounded-md text-lg font-medium font-secondary text-[#2C2643]'>Register</button>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
