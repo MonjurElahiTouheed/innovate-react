349e3dc5bd26eb73c12cea74138a57480e607490
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-05-21 13:00:41 +0600

    made navbar and banner layout responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Container from '../Layout/Container';
 import banner_shape from '../../assets/banner_shape.png'
 import banner from '../../assets/banner.png'
@@ -8,12 +7,12 @@ const Banner = () => {
             backgroundImage: `url(${banner_shape})`
         }}>
             <Container>
-                <div className="text-center">
-                    <h1 className='w-[796px] mx-auto font-primary text-[56px] font-bold leading-[64px] text-white'>Get your work done with Management Tool</h1>
-                    <p className='w-[467px] mx-auto font-secondary text-lg leading-[32px] mt-5 text-white'>The world's first project management platform that connects everything</p>
-                    <div className='mt-8 relative'>
-                        <input className='w-[480px] py-5 border pl-6 bg-white outline-0 placeholder:text-gray-400 text-gray-400 rounded-[6px]' type="text" placeholder='Your business email'/>
-                        <button className='absolute top-1/2 translate-y-[-50%] right-[480px] font-secondary text-lg font-medium py-[15px] px-7 bg-secondary rounded-[6px] text-[#2C2643]'>Try for free</button>
+                <div className="text-center px-4 md:px-0">
+                    <h1 className='md:w-[796px] mx-auto font-primary text-[33px] md:text-[56px] font-bold leading-[64px] text-white'>Get your work done with Management Tool</h1>
+                    <p className='md:w-[467px] mx-auto font-secondary text-base md:text-lg leading-[32px] mt-5 text-white'>The world's first project management platform that connects everything</p>
+                    <div className='mt-8 relative flex flex-col md:block'>
+                        <input className='md:w-[480px] py-5 border pl-6 bg-white outline-0 placeholder:text-gray-400 text-gray-400 rounded-[6px]' type="text" placeholder='Your business email'/>
+                        <button className='md:absolute mt-4 top-1/2 md:translate-y-[-50%] md:right-[480px] font-secondary text-lg font-medium py-[15px] px-7 bg-secondary rounded-[6px] text-[#2C2643]'>Try for free</button>
                     </div>
                     <div className='mt-[98px]'>
                         <img src={banner} alt="#banner.png" />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,26 @@
 import logo from '../../assets/logo.png';
 import Container from '../Layout/Container';
+import { FaBars } from "react-icons/fa6";
 const Navbar = () => {
     return (
         <div className='py-8 absolute w-full'>
             <Container>
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center px-5 md:px-0">
                     <div>
                         <img src={logo} alt="#logo.png" />
                     </div>
-                    <ul className='flex gap-x-[50px] font-secondary text-lg font-medium text-white'>
+                    <ul className='hidden md:flex gap-x-[50px] font-secondary text-lg font-medium text-white'>
                         <li>Home</li>
                         <li>Features</li>
                         <li>Service</li>
                         <li>Pages</li>
                         <li>Blog</li>
                     </ul>
-                    <div className='flex gap-x-9 items-center'>
+                    <div className='hidden md:flex gap-x-9 items-center'>
                         <p className='font-secondary text-lg font-medium text-white'>Login</p>
                         <button className='px-12 py-5 bg-secondary rounded-md text-lg font-medium font-secondary text-[#2C2643]'>Register</button>
                     </div>
+                    <FaBars color='white' size={24} className='md:hidden' />
                 </div>
             </Container>
         </div>
