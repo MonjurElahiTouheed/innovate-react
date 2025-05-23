8e1d28b655123f039a096656fa0a6252cdee2125
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-05-24 04:29:16 +0600

    wrote functionality logic for navbar menubar button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.png';
 import Container from '../Layout/Container';
 import { FaBars } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 const Navbar = () => {
+    const [state, setState] = useState(false);
+    const handleState = () => {
+        setState(!state);
+        console.log('d')
+    }
     return (
         <div className='py-8 absolute w-full'>
             <Container>
@@ -20,8 +27,27 @@ const Navbar = () => {
                         <p className='font-secondary text-lg font-medium text-white'>Login</p>
                         <button className='px-12 py-5 bg-secondary rounded-md text-lg font-medium font-secondary text-[#2C2643]'>Register</button>
                     </div>
-                    <FaBars color='white' size={24} className='md:hidden' />
+                    
+                    {
+                        state ? <IoMdClose onClick={handleState} color='white' size={24} className='md:hidden' /> : <FaBars onClick={handleState} color='white' size={24} className='md:hidden' />
+
+                    }
                 </div>
+
+
+            {
+                state && <ul className='flex flex-col items-center md:hidden py-9 font-secondary text-lg font-medium text-white bg-[#3639A4]'>
+                        <li><a href="" className='font-secondary '>Home</a></li>
+                        <li><a href="" className='font-secondary '>Features</a></li>
+                        <li><a href="" className='font-secondary '>Service</a></li>
+                        <li><a href="" className='font-secondary '>Pages</a></li>
+                        <li><a href="" className='font-secondary '>Blog</a></li>
+                        <hr className='w-1/2 text-white mt-7 mb-2'/>
+                        <li className='mt-3'><button className='px-7 py-2.5 bg-secondary rounded-md text-lg font-medium font-secondary text-[#2C2643]'>Login</button></li>
+                        <li className='mt-4'><a href="#" className='text-base md:text-lg font-medium font-secondary'>Register ?</a></li>
+                    </ul>
+            }
+
             </Container>
         </div>
     );
